750dadb445f07548920432a4e667f7bc5684113d
rjcbn <100301744+rjcbn@users.noreply.github.com>
2023-01-03 17:42:38 +0000

    fixes

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
+  res.status(200);
   res.send('Testing availability...');
-  console.log("Testing console...")
+  console.log("Testing console...");
 });
 
 app.listen(port, () => {
@@ -17,6 +18,7 @@ app.listen(port, () => {
 
 app.post('/',(req,res) => {
   console.log(req.headers);
-  console.log(req.body);
-  res.json(JSON.stringify(req.headers) + ">>>>>>>>>>>>>>>>>>>>" + JSON.stringify(req.body));
+  console.log(JSON.stringify(req.body));
+  res.status(200);
+  res.json("POST RECEIVED");
 });
