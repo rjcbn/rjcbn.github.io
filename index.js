148c000294442cf32d964f343a14fdd7755953fa
rjcbn <100301744+rjcbn@users.noreply.github.com>
2023-01-03 16:01:08 +0000

    support post

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const bodyParser = require("body-parser");
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Testing availability...');
@@ -8,4 +12,9 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
+});
+
+app.post('/',(request,response) => {
+  console.log(request.headers);
+  console.log(request.body);
 });
